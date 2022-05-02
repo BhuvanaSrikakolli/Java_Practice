67805c378e3fe70bb27813dd48fdb3d59c4288ae
Bhuvana.srikakolli <103178212+BhuvanaSrikakolli@users.noreply.github.com>
2022-05-02 17:58:07 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,34 @@
+/* JFM1T4_Assignment4:
+   Write a program to print the following pattern:
+     1 
+     01 
+     101 
+     0101 
+     10101 
+*/
+
+import java.util.Scanner;
+public class Pattern {
+    //main method
+    public static void main(String[] args) {
+        //Declare variable n and initialize value as 5
+        int n ;
+        //Check whether the i and j are equal or not using for loop
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter the n value");
+      n=sc.nextInt();
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+                //If i and j are equal check (i+j)%2==0 or not if zero then print one else print zero
+                if((i+j)%2==0) {
+                    System.out.print("1");
+                }
+                else {
+                    System.out.print("0");
+                }
+            }
+            //printing values in next line
+            System.out.println();
+        }
+    }
+}
